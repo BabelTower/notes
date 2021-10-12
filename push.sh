77342ce1e3f52badfc2dfdb9d57798f34cbe38a4
fangzeyu <2455255381@qq.com>
2021-10-12 21:42:36 +0800

    更新了首页

--- push.sh
+++ push.sh
@@ -6,11 +6,4 @@ cp README.md docs/README.md
 # 更新 master
 git add .
 git commit -m "$message"
-git push -f git@github.com:BabelTower/notes.git master
-
-# 更新 notes
-cd docs/
-git init
-git add -A
-git commit -m "$message"
-git push -f git@github.com:BabelTower/notes.git master:notes
+git push origin master
