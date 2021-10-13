8d2d173dbaa6dff20880c6005d99a7eb6023d220
fangzeyu <2455255381@qq.com>
2021-10-13 14:21:55 +0800

    add命令行交界面

--- bash.sh
+++ bash.sh
@@ -1,9 +1,17 @@
-echo "[0] 更新Github仓库"
-echo "[1] 启动local serve"
+echo " --------------------------------------------"
+echo "|                                            |"
+echo "|          [0] update Github repo            |"
+echo "|          [1] run local serve               |"
+echo "|                                            |"
+echo " --------------------------------------------"
 read -p "Enter number : " func_num
 if [ $func_num -eq 0 ];
 then
-echo 
 read -p "Enter commit name : " commit_name
 ./push.sh $commit_name
+fi
+
+if [ $func_num -eq 1 ];
+then
+docsify serve docs
 fi
--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 message=$1
 
 # 复制 README.md
-cp docs/README.md README.md
+cp README.md docs/README.md
 
 # 更新 master
 git add .
